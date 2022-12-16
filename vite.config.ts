73d9dbd9695992033338bad616b4fd0caa15a1cf
Burt <1020103647@qq.com>
2022-12-16 20:24:30 +0800

    build(vite): add server & build config

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,41 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // 把 @ 指向到 src 目录去
     },
   },
+  build: {
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 2000,
+    // 在生产环境移除console.log
+    terserOptions: {
+      compress: {
+        drop_console: false,
+        pure_funcs: ['console.log', 'console.info'],
+        drop_debugger: true,
+      },
+    },
+    assetsDir: 'static/assets',
+    // 静态资源打包到dist下的不同目录
+    // rollupOptions: {
+    //   output: {
+    //     chunkFileNames: 'static/js/[name]-[hash].js',
+    //     entryFileNames: 'static/js/[name]-[hash].js',
+    //     assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+    //   },
+    // },
+  },
+  server: {
+    host: true, // host设置为true才可以使用network的形式，以ip访问项目
+    port: 8080, // 端口号
+    open: true, // 自动打开浏览器
+    cors: true, // 跨域设置允许
+    strictPort: true, // 如果端口已占用直接退出
+    // 接口代理
+    proxy: {
+      '/api': {
+        // 本地 8000 前端代码的接口 代理到 8888 的服务端口
+        target: 'http://localhost:8888/',
+        changeOrigin: true, // 允许跨域
+        rewrite: (path) => path.replace('/api/', '/'),
+      },
+    },
+  },
 })
