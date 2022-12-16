2390301e3eae8800cd976cd7bae3e46152f20064
Burt <1020103647@qq.com>
2022-12-16 21:43:20 +0800

    feat(VueUse): add VueUse

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useLocalStorage, useMouse, usePreferredDark } from '@vueuse/core'
+
+// tracks mouse position
+const { x, y } = useMouse()
+
+// is user prefers dark theme
+const isDark = usePreferredDark()
+
+// persist state in localStorage
+const store = useLocalStorage('my-storage', {
+  name: 'Apple',
+  color: 'red',
+})
+</script>
 
 <template>
   <h1>About</h1>
+  <div>mouse: x:{{ x }} - y:{{ y }}</div>
+  <div>isDark: {{ isDark }}</div>
+  <div>{{ store }}</div>
 </template>
 
 <style scoped>
