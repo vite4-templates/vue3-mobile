889d83c79d8bc5080f8ab5a77ce20b1b92febaa1
Burt <1020103647@qq.com>
2022-12-16 20:20:00 +0800

    build(alias): add @ alias

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { testAlias } from '@/utils/index'
 </script>
 
 <template>
+  {{ testAlias }}
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://vites.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
@@ -16,13 +18,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 .logo {
-  height: 6em;
   padding: 1.5em;
+  height: 6em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
