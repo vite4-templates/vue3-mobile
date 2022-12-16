cb8eed58bec7d56b6b95e4d633e2d2011bfa145a
Burt <1020103647@qq.com>
2022-12-16 16:55:49 +0800

    build(prettier): add prettier && update eslint && do format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {}
+  rules: {},
 }
